db5591ad59f0f7adb3ef47f86c5d8ed4a710705b
zh9314 <h.zhou@uva.nl>
2017-02-03 10:30:20 +0100

    1.Change the system input method with JLine; \n2.Add a function 'copy';\n3.Fix some bugs.

--- src/PWDMain.java
+++ src/PWDMain.java
@@ -1,3 +1,8 @@
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 import java.io.Console;
 import java.io.File;
 import java.io.FileWriter;
@@ -5,41 +10,40 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+import jline.*;
 
 public class PWDMain {
 	
 	public static PWDBook pb;
 	
-	public static void initAPP(){
+	public static void initAPP() throws IOException{
 		System.out.println("Welcome to use the pwd book! \n          ----Developped by Huan.");
 		String curDir = new PWDMain().getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
 		int index = curDir.lastIndexOf('/');
 		String pwdBookPath = curDir.substring(0, index+1)+"book.pwd";
-		File bookFile = new File(pwdBookPath);
-		Scanner sc = new Scanner(System.in);  
-        sc.useDelimiter("\n");  
+		File bookFile = new File(pwdBookPath);  
 		if(!bookFile.exists()){
-			System.out.println("The pwd book doesn't exist! Do you need to create one?(y/n)");
-			while(sc.hasNext()){
-				String input = sc.next();
-				if(input.equals("y")){
+			ConsoleReader reader = new ConsoleReader();
+			String input = "";
+			System.out.println("The pwd book doesn't exist!");
+			while((input = reader.readLine("Do you need to create one?(y/n) ")) != null){
+				if(input.toLowerCase().trim().equals("y")){
 					String pwd = initPWD();
 					pb = new PWDBook(pwdBookPath, pwd);
 					pb.createPWDBook();
 					break;
-				}else if(input.equals("n")){
+				}else if(input.toLowerCase().trim().equals("n")){
 					System.out.println("OK!EXIT!");
 					System.exit(-1);
 				}else{
-					System.out.print("Invalid input! Jsut choose to input 'y' or 'n'!\ny/n? : ");
+					System.out.print("Invalid input! Jsut choose to input 'y' or 'n'!\n");
 				}
 			}
 		}else
 		{
 			while(true){
-				System.out.print("Please input the unified password:");
-				Console console = System.console();  
-		        String pwd = new String(console.readPassword());
+				ConsoleReader reader = new ConsoleReader();
+				String pwd = reader.readLine("Please input the unified password: ", '*');
 				pb = new PWDBook(pwdBookPath, pwd);
 				if(pb.initBookPWDs(pwd))
 					break;
@@ -51,13 +55,11 @@ public class PWDMain {
 		}
 	}
 	
-	public static String initPWD(){
+	public static String initPWD() throws IOException{
 		while(true){
-			System.out.print("Please initialize the unified password:");
-			Console console = System.console();  
-	        String pwd = new String(console.readPassword()); 
-	        System.out.print("Retype the password:");
-	        String rpwd = new String(console.readPassword()); 
+			ConsoleReader reader = new ConsoleReader();
+	        String pwd = reader.readLine("Please initialize the unified password: ", '*');
+	        String rpwd = reader.readLine("Retype the password: ", '*'); 
 	        if(pwd.equals(rpwd)){
 	        	System.out.println("Password is set correctly!");
 	        	return pwd;
@@ -84,27 +86,31 @@ public class PWDMain {
 	}
 	
 	public static boolean isInteger(String str) {    
+		if(str.equals(""))
+			return false;
 	    Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");    
 	    return pattern.matcher(str).matches();    
 	  } 
 	
-	public static String generatePWD(){
-		System.out.print("Please enter the number of digits that you want to generate(1-99): ");
-		Scanner sc = new Scanner(System.in);  
-        sc.useDelimiter("\n"); 
-        String pwdString = "";
-        while(sc.hasNext()){
-        	String num = sc.next();
-            if(!isInteger(num))
-            	System.out.print("Invalid number, enter again: ");
+	public static String generatePWD() throws IOException{
+		ConsoleReader reader = new ConsoleReader();
+		String pwdString = "";
+		String num = "";
+        while((num = reader.readLine("Please enter the number of digits that you want to generate(1-99): ")) != null){
+            if(!isInteger(num.trim()))
+            	System.out.println("Invalid number, enter again!");
             else{
-            	int n = Integer.parseInt(num);
+            	int n = Integer.parseInt(num.trim());
             	pwdString = pb.generatePWD(n);
             	if(pwdString != null){
             		System.out.println("PWD generated->  "+pwdString);
+            		Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();  
+			        Transferable tText = new StringSelection(pwdString);  
+			        clip.setContents(tText, null); 
+			        System.out.println("This generated password has been copied to system clipboard!");
             		break;
             	}else{
-            		System.out.print("The digits number should be in the range of 0~99: ");
+            		System.out.println("The digits number should be in the range of 0~99! ");
             	}
             }
         }
@@ -112,87 +118,100 @@ public class PWDMain {
         
 	}
 	
-	public static void addPWD(String [] args){
+	public static void addPWD(String [] args) throws IOException{
 		if(args.length == 1){
-			System.out.print("Please enter the account names(separate them with '::'): ");
-			Scanner sc = new Scanner(System.in);  
-	        sc.useDelimiter("\n"); 
-	        String accounts = sc.next();
-	        System.out.print("Please enter some tags for the accounts(separate them with '::'): ");
-			String tags = sc.next();
-			System.out.print("Please enter a description for this record: ");
-			String des = sc.next();
+			ConsoleReader reader = new ConsoleReader();
+			String accounts = "";
+			while(true){
+				accounts = reader.readLine("Please enter the account names(separate them with '::'): ");
+				if(accounts.trim().equals(""))
+					System.out.println("The filed of account cannot be empty!");
+				else
+					break;
+			}
+			String tags = "";
+			while(true){
+				tags = reader.readLine("Please enter some tags for the accounts(separate them with '::'): ");
+				if(tags.trim().equals(""))
+					System.out.println("The filed of tags cannot be empty!");
+				else
+					break;
+			}
+			String des = reader.readLine("Please enter a description for this record: ");
 			if(des.equals(""))
 				des = "null";
 			String pwdString = "";
-			System.out.print("Generate a password? (y/n) ");
-			while(sc.hasNext()){
-				String ans = sc.next();
-				if(ans.toLowerCase().equals("y")){
+			String ans = "";
+			while((ans = reader.readLine("Generate a password? (y/n) ")) != null){
+				if(ans.toLowerCase().trim().equals("y")){
 					pwdString = generatePWD();
 					break;
-				}else if(ans.toLowerCase().equals("n")){
-					System.out.print("Please type in password: ");
-					pwdString = sc.next();
-					while(true){
-						if(!pwdString.contains("%$$%") && !pwdString.contains("::"))
+				}else if(ans.toLowerCase().trim().equals("n")){
+					while((pwdString = reader.readLine("Please type in password: ")) != null){
+						if(pwdString.equals("")){
+							System.out.println("Password cannot be empty!");
+						}else if(pwdString.contains("%$$%")){
+							System.out.println("Password cannot contain '%$$%'!");
+						}else if(pwdString.contains("::")){
+							System.out.println("Password cannot contain '::'!");
+						}else if(pwdString.contains(" ")){
+							System.out.println("Password cannot contain spaces!");
+						}else{
 							break;
-						else{
-							System.out.print("Password cannot contains '%$$%' or '::'!\nPlease type in password: ");
-							pwdString = sc.next();
 						}
 					}
 					break;
 				}else
-					System.out.print("Please just type in 'y' or 'n'!\n y/n? : ");
+					System.out.print("Please just type in 'y' or 'n'!\n");
 			}
 			pb.addPWD(accounts, pwdString, tags, des, "null");
 		}else
 			System.out.println("Too many arguments for 'add'!");
 	}
 	
-	public static void changePWD(String [] args){
+	public static void changePWD(String [] args) throws IOException{
 		if(args.length != 2){
 			System.out.println("The command is wrong!");
 			return ;
 		}
+		boolean changed = false;
 		for(int i = 0 ; i<pb.accounts.size() ; i++){
 			String tagsString = "";
 			for(int j = 0 ; j<pb.accounts.get(i).tags.length ; j++)
 				tagsString += pb.accounts.get(i).tags[j]+"    ";
 			if(tagsString.toLowerCase().contains(args[1].toLowerCase())){
-				System.out.println("Change password of this?");
+				System.out.println("\nChange password of this?");
 				System.out.println("Tags: "+tagsString);
 				System.out.println("Description: "+pb.accounts.get(i).description);
-				System.out.print("y/n? : ");
-				Scanner sc = new Scanner(System.in);  
-		        sc.useDelimiter("\n"); 
-		        String pwdString = "";
-		        while(sc.hasNext()){
-					String ans = sc.next();
-					if(ans.toLowerCase().equals("y")){
-						System.out.print("Generate a password? (y/n) ");
-						while(sc.hasNext()){
-							ans = sc.next();
-							if(ans.toLowerCase().equals("y")){
+				ConsoleReader reader = new ConsoleReader();
+		        String pwdString = "", ans = "";
+		        while((ans = reader.readLine("y/n? : ")) != null){
+					if(ans.toLowerCase().trim().equals("y")){
+						while((ans = reader.readLine("Generate a password? (y/n) ")) != null){
+							if(ans.toLowerCase().trim().equals("y")){
 								pwdString = generatePWD();
 								break;
-							}else if(ans.toLowerCase().equals("n")){
-								System.out.print("Please type in password: ");
-								pwdString = sc.next();
-								while(true){
-									if(!pwdString.contains("%$$%") && !pwdString.contains("::"))
+							}else if(ans.toLowerCase().trim().equals("n")){
+								while((pwdString = reader.readLine("Please type in password: ")) != null){
+									if(pwdString.equals("")){
+										System.out.println("Password cannot be empty!");
+									}else if(pwdString.contains("%$$%")){
+										System.out.println("Password cannot contain '%$$%'!");
+									}else if(pwdString.contains("::")){
+										System.out.println("Password cannot contain '::'!");
+									}else if(pwdString.contains(" ")){
+										System.out.println("Password cannot contain spaces!");
+									}else{
 										break;
-									else{
-										System.out.print("Password cannot contains '%$$%' or '::'!\nPlease type in password: ");
-										pwdString = sc.next();
 									}
 								}
 								break;
 							}else
-								System.out.print("Please just type in 'y' or 'n'!\ny/n? : ");
+								System.out.print("Please just type in 'y' or 'n'!\n");
 						}
 						pb.accounts.get(i).pwd = pwdString;
+						System.out.println("The password has been changed!");
+						changed = true;
 						break;
 					}else if(ans.toLowerCase().equals("n")){
 						System.out.println("Try to find another record containing "+args[1]);
@@ -200,40 +219,88 @@ public class PWDMain {
 					}else
 						System.out.print("Please just type in 'y' or 'n'!\ny/n? : ");
 				}
+		        
+		        if(changed)
+		        	break;
 			}
 		}
+		if(!changed)
+			System.out.println("There are no more records with tag "+args[1]);
 	}
 	
 	
-	public static void deletePWD(String [] args){
+	public static void copyPWD(String [] args) throws IOException{
 		if(args.length != 2){
 			System.out.println("The command is wrong!");
 			return ;
 		}
+		boolean copied = false;
 		for(int i = 0 ; i<pb.accounts.size() ; i++){
 			String tagsString = "";
 			for(int j = 0 ; j<pb.accounts.get(i).tags.length ; j++)
 				tagsString += pb.accounts.get(i).tags[j]+"    ";
 			if(tagsString.toLowerCase().contains(args[1].toLowerCase())){
-				System.out.println("Delete password of this?");
+				System.out.println("\nCopy the password of this?");
 				System.out.println("Tags: "+tagsString);
 				System.out.println("Description: "+pb.accounts.get(i).description);
-				System.out.print("y/n? : ");
-				Scanner sc = new Scanner(System.in);  
-		        sc.useDelimiter("\n");
-		        while(sc.hasNext()){
-					String ans = sc.next();
-					if(ans.toLowerCase().equals("y")){
-						pb.accounts.remove(i);
+				ConsoleReader reader = new ConsoleReader();
+				String ans = "";
+		        while((ans = reader.readLine("y/n? : ")) != null){
+					if(ans.toLowerCase().trim().equals("y")){
+						String pwdString = pb.accounts.get(i).pwd;
+						Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();  
+				        Transferable tText = new StringSelection(pwdString);  
+				        clip.setContents(tText, null); 
+				        System.out.println("The password has been copied to system clipboard!");
+				        copied = true;
+				        break;
+					}else if(ans.toLowerCase().trim().equals("n")){
+						System.out.println("Try to find another record containing "+args[1]);
 						break;
-					}else if(ans.toLowerCase().equals("n")){
+					}else
+						System.out.print("Please just type in 'y' or 'n'!\n");
+				}
+		        if(copied)
+		        	break;
+			}
+		}if(!copied)
+			System.out.println("There are no more records with tag "+args[1]);
+	}
+	
+	
+	public static void deletePWD(String [] args) throws IOException{
+		if(args.length != 2){
+			System.out.println("The command is wrong!");
+			return ;
+		}
+		boolean deleted = false;
+		for(int i = 0 ; i<pb.accounts.size() ; i++){
+			String tagsString = "";
+			for(int j = 0 ; j<pb.accounts.get(i).tags.length ; j++)
+				tagsString += pb.accounts.get(i).tags[j]+"    ";
+			if(tagsString.toLowerCase().contains(args[1].toLowerCase())){
+				System.out.println("\nDelete password of this?");
+				System.out.println("Tags: "+tagsString);
+				System.out.println("Description: "+pb.accounts.get(i).description);
+				ConsoleReader reader = new ConsoleReader();
+				String ans = "";
+		        while((ans = reader.readLine("y/n? : ")) != null){
+					if(ans.toLowerCase().trim().equals("y")){
+						pb.accounts.remove(i);
+						System.out.println("The record has been removed!");
+						deleted = true;
+						break;
+					}else if(ans.toLowerCase().trim().equals("n")){
 						System.out.println("Try to find another record!");
 						break;
 					}else
-						System.out.print("Please just type in 'y' or 'n'!\ny/n? : ");
+						System.out.print("Please just type in 'y' or 'n'!\n");
 				}
+		        if(deleted)
+		        	break;
 			}
-		}
+		}if(!deleted)
+			System.out.println("There are no more records with tag "+args[1]);
 	}
 	
 	public static void savePWD(String [] args){
@@ -242,15 +309,14 @@ public class PWDMain {
 			return ;
 		}
 		pb.saveBook();
+		System.out.println("The pwd book has been updated!");
 	}
 	
-	public static void processCMDs(){
-		System.out.print("Please enter the command of your option(list\\add\\change\\delete\\save\\exit -h for help):\n");
-		Scanner sc = new Scanner(System.in);  
-        sc.useDelimiter("\n|\r"); 
+	public static void processCMDs() throws IOException{
+		System.out.print("Please enter the command of your option(list\\add\\change\\delete\\save\\copy\\exit -h for help):\n");
         while(true){
-    		System.out.print("PWDBook CMD>>");
-        	String input = sc.nextLine();
+    		ConsoleReader reader = new ConsoleReader();
+    		String input = reader.readLine("PWDBook CMD>>");
         	if(input.trim().equals("")){
         		continue;
         	}
@@ -276,6 +342,8 @@ public class PWDMain {
         		deletePWD(args);
         	}else if(cmd.equals("save")){
         		savePWD(args);
+        	}else if(cmd.equals("copy")){
+        		copyPWD(args);
         	}
         	else{
         		System.out.println("Invalid commands!");
@@ -284,7 +352,7 @@ public class PWDMain {
         }
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		initAPP();
 		processCMDs();
